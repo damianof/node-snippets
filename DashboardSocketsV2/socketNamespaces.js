09ce162d25307d5a32b42adbd8cd5857895d559f
Damiano Fusco <damiano.fusco@nielsen.com>
2015-06-17 15:51:48 -0400

    Improved socket.io namespaces example

--- DashboardSocketsV2/socketNamespaces.js
+++ DashboardSocketsV2/socketNamespaces.js
@@ -1,3 +1,14 @@
+/*
+Restricting yourself to a namespace
+If you have control over all the messages and events emitted for a particular application, 
+using the default / namespace works. If you want to leverage 3rd-party code, or produce code 
+to share with others, socket.io provides a way of namespacing a socket.
+
+This has the benefit of multiplexing a single connection. Instead of 
+socket.io using two WebSocket connections, it’ll use one.
+*/
+
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -19,24 +30,42 @@ app.use(express.static(__dirname + '/client'));
 //var io = require('socket.io')(3333);
 var chat = io
 	.of('/chat')
-	.on('connection', function (socket) {
+	.on('connection', function (chatSocket) {
 		console.log('/chat a user connected');
-		socket.emit('message', {
-				that: 'only'
-			, '/chat': 'will get'
+
+		chat.on('disconnect', function(){
+			console.log('/chat a user disconnected');
 		});
+
+		// chatSocket.emit('message', {
+		// 		that: 'only'
+		// 	, '/chat': 'will get'
+		// });
 		chat.emit('message', {
 				everyone: 'in'
 			, '/chat': 'will get'
+		});
+
+		chatSocket.on('reply', function (item) {
+			console.log('/chat server received reply', item);
 		});
 	});
 
 var news = io
 	.of('/news')
-	.on('connection', function (socket) {
+	.on('connection', function (newsSocket) {
 		console.log('/news a user connected');
-		socket.emit('item', { 
+
+		news.on('disconnect', function(){
+			console.log('/news a user disconnected');
+		});
+
+		news.emit('item', { 
 			news: 'item' 
+		});
+
+		newsSocket.on('reply', function (item) {
+			console.log('/NEWS server received reply', item);
 		});
 	});
 
